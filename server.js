429351c1fc088070bc49bf4ffe744ce044747fbc
Nikolai Petukhov <kolya.petukhov.03@mail.ru>
2024-10-26 13:43:16 +0300

    back to http

--- server.js
+++ server.js
@@ -1,17 +1,8 @@
 const express = require('express');
 const path = require('path');
-const https = require('https');
-const http = require('http');
-const fs = require('fs');
 
 const app = express();
-const PORT = 443;
-
-// SSL options
-const options = {
-    key: fs.readFileSync('server.key'),
-    cert: fs.readFileSync('server.cert')
-};
+const PORT = 80;
 
 app.use(express.static(path.join(__dirname, 'build')));
 
@@ -19,12 +10,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-https.createServer(options, app).listen(PORT, () => {
-    console.log(`Secure server is running on ${PORT}`);
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
-
-// redirect HTTP to HTTPS
-http.createServer((req, res) => {
-    res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
-    res.end();
-}).listen(80);
