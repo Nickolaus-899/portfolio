00ea31ff826b5a0d1d49d13d428cfef0e7ec732f
Nikolai Petukhov <kolya.petukhov.03@mail.ru>
2024-10-30 23:08:56 +0300

    fixed a problem with background color of project cards

--- src/components/projects_list/ProjectCard.jsx
+++ src/components/projects_list/ProjectCard.jsx
@@ -6,17 +6,19 @@ const ProjectCard = (props) => {
     const { short, favicon } = extra;
 
   return (
-      <a href={project.path} className={`project-card smooth-appear ${hover ? hover : ''}`}>
-          <div className="card-title">
-              {favicon ? (
-                  <img src={favicon} alt={'Project icon'}/>
-              ) : null}
+      <div className="project-item-wrapper smooth-appear">
+          <a href={project.path} className={`project-card ${hover ? hover : ''}`}>
+              <div className="card-title">
+                  {favicon ? (
+                      <img src={favicon} alt={'Project icon'}/>
+                  ) : null}
 
-              <h3 className="mclaren-regular">{project.name}</h3>
-          </div>
+                  <h3 className="mclaren-regular">{project.name}</h3>
+              </div>
 
-          <div>{short}</div>
-      </a>
+              <div>{short}</div>
+          </a>
+      </div>
   );
 };
 
