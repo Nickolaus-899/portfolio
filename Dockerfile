b3e244fead43089b34a8bf7ea506f72369615bc8
Nickolaus-899 <kolya.petukhov.03@mail.ru>
2025-05-11 13:47:04 +0300

    docker

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM node:16-slim
+
+WORKDIR /app
+
+COPY package.json .
+COPY package-lock.json .
+
+COPY keys keys
+
+RUN npm install
+
+COPY build build
+
+COPY server.js .
+
+EXPOSE 443
+CMD [ "node", "server.js" ]
--- server.js
+++ server.js
@@ -7,9 +7,12 @@ const app = express();
 const HTTP_PORT = 80; // HTTP
 const HTTPS_PORT = 443; // HTTPS
 
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/nikolaus-portfolio.online/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('/etc/letsencrypt/live/nikolaus-portfolio.online/cert.pem', 'utf8');
-const ca = fs.readFileSync('/etc/letsencrypt/live/nikolaus-portfolio.online/chain.pem', 'utf8');
+const KEY_PATH = 'keys'
+// /etc/letsencrypt/live/nikolaus-portfolio.online
+
+const privateKey = fs.readFileSync(`${KEY_PATH}/privkey.pem`, 'utf8');
+const certificate = fs.readFileSync(`${KEY_PATH}/cert.pem`, 'utf8');
+const ca = fs.readFileSync(`${KEY_PATH}/chain.pem`, 'utf8');
 
 const credentials = { key: privateKey, cert: certificate, ca: ca };
 
